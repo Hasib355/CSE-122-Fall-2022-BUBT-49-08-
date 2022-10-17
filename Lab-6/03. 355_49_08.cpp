b3734edd2fd67b586523cc4f850d592fdd63d0bb
Shah Md Hasibur Rahaman Hasib <106367768+Hasib355@users.noreply.github.com>
2022-10-17 15:15:51 +0600

    Add files via upload

---
+++ Lab-6/03. 355_49_08.cpp
@@ -0,0 +1,24 @@
+//Codeforces
+//Problem No : 977A
+//Problem Name : Word Substraction
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int n;
+    int k;
+    cin>>n;
+    cin>>k;
+    for(int i=0;i<k;i++){
+        if(n%10==0){
+            n/=10;
+            continue;
+        }
+        n--;
+    }
+    cout<<n;
+}
